80c98ce67b41f9cae9ddfe57dcea27ca1fa24205
Jaspreet Singh <Jaspreet.singh251097@gmail.com>
2022-03-27 23:51:00 +0530

    Added files

---
+++ SuperDepartment.java
@@ -0,0 +1,18 @@
+class SuperDepartment {
+    public String departmentName() {
+        return "Super Department ";
+    }
+
+    public String getTodaysWork() {
+        return "No Work as of now";
+    }
+
+    public String getWorkDeadline() {
+        return "Nil";
+    }
+
+    public String IsTodayAHoliday() {
+        return "Today is not a holiday";
+    }
+
+}
